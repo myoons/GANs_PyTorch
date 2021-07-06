3f6fd64f8c5d831cc289cb1d4f9eeea08643f3d8
myoons <grandchasevs@kaist.ac.kr>
2021-07-07 01:27:37 +0900

    Add dataset for DCGAN

---
+++ DCGAN/dataset.py
@@ -0,0 +1,19 @@
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+class SimpleDataset(Dataset):
+    def __init__(self, images):
+        self.images = images
+
+        self.transforms = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        ])
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        return self.transforms(self.images[idx])
